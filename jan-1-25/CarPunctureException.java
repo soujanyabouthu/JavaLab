47b85803ee86161731215f115ebda4394d4af414
soujanyabouthu <srinivasbouthu6@gmail.com>
2025-01-06 12:07:52 +0530

    Update CarPunctureException.java

--- jan-1-25/CarPunctureException.java
+++ jan-1-25/CarPunctureException.java
@@ -1,1 +1,14 @@
 
+
+
+////BLC class of CarUser -> CarPunctureException.java
+
+package jan_1_25;
+
+public class CarPunctureException extends Exception
+{
+public CarPunctureException(String puncture)
+{
+	super(puncture);
+}
+}
