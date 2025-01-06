95c152e0adcf4764f1791ce06ff2efac76da2122
soujanyabouthu <srinivasbouthu6@gmail.com>
2025-01-06 12:06:48 +0530

    Update CarHeatException.java

--- jan-1-25/CarHeatException.java
+++ jan-1-25/CarHeatException.java
@@ -1,1 +1,15 @@
 # JavaLab
+
+
+
+//BLC class of CarUser -> CarHeatException.java
+
+package jan_1_25;
+
+public class CarHeatException extends Exception
+{
+	public CarHeatException(String heat)
+	{
+		super(heat);
+	}
+}
